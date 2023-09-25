d79ff4b4849f91a1644d6ee372786cdae79545b0
Sheldon <kasebrot@live.com>
2023-09-25 16:46:44 -0500

    Added styles...
    Background color and uppercase text.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import { Greeting } from './Greeting';
 
 const App = () => {
-  return (
-    <>
-      <Greeting name="Adv Cloud Class" favoriteNumber={7} favoriteColors={["blue", "green"]} />
-      <Greeting name="Bob" favoriteNumber={2} />
-      <Greeting name="Sam" favoriteNumber={5} />
-      <Greeting name="Billie" favoriteNumber={3} />
-    </>
+
+    const styles = {
+      container: {
+        padding: 20,
+        backgroundColor: new Date().getSeconds() % 2 === 0 ? "blue" : "green",
+        textTransform: 'uppercase'
+      }
+    };
+
+    return (
+        <div style={styles.container}>
+            <Greeting name="Adv Cloud Class" favoriteNumber={7} favoriteColors={["blue", "green"]} />
+            <Greeting name="Bob" favoriteNumber={2} />
+            <Greeting name="Sam" favoriteNumber={5} />
+            <Greeting name="Billie" favoriteNumber={3} />
+        </div>
   );
 }
 
--- src/Greeting.js
+++ src/Greeting.js
@@ -1,20 +1,19 @@
 
 
 
-export const Greeting = (props) => {
-    console.log(props);
+export const Greeting = ({name, favoriteNumber, favoriteColors}) => {
     return (
         <>
             <h2>
-                Hello, {props.name}!
+                Hello, {name}!
             </h2>
             <h3>
-                {props.name}'s favorite number plus 7 is {props.favoriteNumber + 7}.
+                {name}'s favorite number plus 7 is {favoriteNumber + 7}.
             </h3>
             <h3>
-                {props.name}'s favorite colors are:
+                {name}'s favorite colors are:
                 <ul>
-                    {props.favoriteColors && props.favoriteColors.map(color => <li key={color}>{color}</li>)}
+                    {favoriteColors && favoriteColors.map(color => <li key={color}>{color}</li>)}
                 </ul>
             </h3>
         </>
