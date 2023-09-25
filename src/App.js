4c7b5f34a2f6fe88b8e5965ea10eea62e6c7e661
Sheldon <kasebrot@live.com>
2023-09-25 16:02:45 -0500

    Added additional jsx and using react fragment.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import './App.css';
 
 const App = () => {
   return (
+    <>
     <h1>
       Hello, there!
     </h1>
+    <h2>
+      Hello, there!
+    </h2>
+    <h2>
+      Hello, there!
+    </h2>
+    <h2>
+      Hello, there!
+    </h2>
+    </>
   );
 }
 
